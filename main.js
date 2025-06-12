9edcaa57b73e3a5b7ac83a4196f433107497c2a7
sawbeenn <112643656+sawbeenn@users.noreply.github.com>
2025-06-12 09:03:22 +0545

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U SO MUCH RITIKA ❤️').split('')
+    const titles = ('For U Ms.Pragati ❤️').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
